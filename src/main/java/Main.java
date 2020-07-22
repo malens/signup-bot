a4d69399686bdc4e2c932c65caf92da940b75e85
malens <wilkoszk@gmail.com>
2020-07-22 17:53:08 +0200

    fixed args

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.beust.jcommander.IStringConverter;
 import com.beust.jcommander.JCommander;
 import commands.CreateGroupCommand;
 import commands.HelpCommand;
@@ -5,9 +6,16 @@ import database.DatabaseUtil;
 import main.Args;
 import main.Bot;
 import main.StateStorage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+
 
 
 public class Main {
+
+    private static String apikey;
 
     public static void main(String[] args) {
         Args argss = new Args();
@@ -16,11 +24,22 @@ public class Main {
                 .build()
                 .parse(args);
 
+        try {
+            InputStream reader = new FileInputStream(argss.apikey);
+            BufferedReader br = new BufferedReader(new InputStreamReader(reader));
+            Main.apikey = br.readLine();
+            br.close();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         DatabaseUtil.connectToDatabase("gw2botdb");
         StateStorage.playerMap = DatabaseUtil.getPlayers();
         StateStorage.signUpMap = DatabaseUtil.getSignUps();
         StateStorage.serverMap = DatabaseUtil.getServers();
-        Bot bot = new Bot(argss.apikey)
+        Bot bot = new Bot(Main.apikey)
                 .withCommand(
                         new HelpCommand(),
                         "help"
@@ -32,6 +51,7 @@ public class Main {
                 .subscribe()
                 .build();
     }
+
 
 
 
--- src/main/java/main/Args.java
+++ src/main/java/main/Args.java
@@ -1,8 +1,24 @@
 package main;
 
+import com.beust.jcommander.IStringConverter;
 import com.beust.jcommander.Parameter;
 
+import java.io.File;
+
+
 public class Args {
-    @Parameter(names = "-apikey")
-    public String apikey;
+    @Parameter(names = "-apikey", converter = FileConverter.class)
+    public File apikey;
+
+    public class FileConverter implements IStringConverter<File> {
+        @Override
+        public File convert(String value) {
+            return new File(value);
+        }
+    }
 }
+
+
+
+
+
